766fd7d053ab0f6baa1853581e3c86f2e95e4c0c
Rainfall <rainfey0+git@gmail.com>
2020-10-30 21:36:08 +0000

    Add cores (PARTIAL)

---
+++ Cores.lua
@@ -0,0 +1,18 @@
+--@class Cores
+
+Cores = {
+    DynamicXS = {Mass=70.89, Size=2, Type="Dynamic", Class="XS"},
+    DynamicS = {Mass=375.97, Size=4, Type="Dynamic", Class="S"},
+    DynamicM = {Mass=1980, Size=8, Type="Dynamic", Class="M"},
+    DynamicL = {Mass=12140, Size=16, Type="Dynamic", Class="L"},
+
+    SpaceXS = {Mass=38.99, Size=2, Type="Space", Class="XS"},
+    SpaceS = {Mass=459.57, Size=4, Type="Space", Class="S"},
+    SpaceM = {Mass=3040, Size=8, Type="Space", Class="M"},
+    SpaceL = {Mass=7680, Size=16, Type="Space", Class="L"},
+
+    StaticXS = {Mass=70.89, Size=2, Type="Static", Class="XS"},
+    StaticS = {Mass=360.18, Size=4, Type="Static", Class="S"},
+    StaticM = {Mass=1930, Size=8, Type="Static", Class="M"},
+    StaticL = {Mass=10066.3, Size=16, Type="Static", Class="L"},
+}
