4e29f67f8868e58611bc49892ebe19540da78c9e
Rainfall <rainfey0+git@gmail.com>
2020-12-09 20:10:00 +0000

    Add build volume size

--- Cores.lua
+++ Cores.lua
@@ -1,18 +1,18 @@
 --@class Cores
 
 Cores = {
-    DynamicXS = {Mass=70.89, Size=2, Type="Dynamic", Class="XS"},
-    DynamicS = {Mass=375.97, Size=4, Type="Dynamic", Class="S"},
-    DynamicM = {Mass=1980, Size=8, Type="Dynamic", Class="M"},
-    DynamicL = {Mass=12140, Size=16, Type="Dynamic", Class="L"},
+    DynamicXS = {Mass=70.89, CoreSize=2, BuildSize=16, Type="Dynamic", Class="XS"},
+    DynamicS = {Mass=375.97, CoreSize=4, BuildSize=32, Type="Dynamic", Class="S"},
+    DynamicM = {Mass=1980, CoreSize=8, BuildSize=64, Type="Dynamic", Class="M"},
+    DynamicL = {Mass=12140, CoreSize=16, BuildSize=128, Type="Dynamic", Class="L"},
 
-    SpaceXS = {Mass=38.99, Size=2, Type="Space", Class="XS"},
-    SpaceS = {Mass=459.57, Size=4, Type="Space", Class="S"},
-    SpaceM = {Mass=3040, Size=8, Type="Space", Class="M"},
-    SpaceL = {Mass=7680, Size=16, Type="Space", Class="L"},
+    SpaceXS = {Mass=38.99, CoreSize=2, BuildSize=16, Type="Space", Class="XS"},
+    SpaceS = {Mass=459.57, CoreSize=4, BuildSize=32, Type="Space", Class="S"},
+    SpaceM = {Mass=3040, CoreSize=8, BuildSize=64, Type="Space", Class="M"},
+    SpaceL = {Mass=7680, CoreSize=16, BuildSize=128, Type="Space", Class="L"},
 
-    StaticXS = {Mass=70.89, Size=2, Type="Static", Class="XS"},
-    StaticS = {Mass=360.18, Size=4, Type="Static", Class="S"},
-    StaticM = {Mass=1930, Size=8, Type="Static", Class="M"},
-    StaticL = {Mass=10066.3, Size=16, Type="Static", Class="L"},
+    StaticXS = {Mass=70.89, CoreSize=2, BuildSize=16, Type="Static", Class="XS"},
+    StaticS = {Mass=360.18, CoreSize=4, BuildSize=32, Type="Static", Class="S"},
+    StaticM = {Mass=1930, CoreSize=8, BuildSize=64, Type="Static", Class="M"},
+    StaticL = {Mass=10066.3, CoreSize=16, BuildSize=128, Type="Static", Class="L"},
 }
