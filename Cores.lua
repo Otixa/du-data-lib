f38c371088bc03435864e3c4c42c83aa4b853172
Rainfall <rainfey0+git@gmail.com>
2020-12-09 20:25:49 +0000

    Update dynamic M core mass

--- Cores.lua
+++ Cores.lua
@@ -3,7 +3,7 @@
 Cores = {
     DynamicXS = {Mass=70.89, CoreSize=2, BuildSize=16, Type="Dynamic", Class="XS"},
     DynamicS = {Mass=375.97, CoreSize=4, BuildSize=32, Type="Dynamic", Class="S"},
-    DynamicM = {Mass=1980, CoreSize=8, BuildSize=64, Type="Dynamic", Class="M"},
+    DynamicM = {Mass=1984.6, CoreSize=8, BuildSize=64, Type="Dynamic", Class="M"},
     DynamicL = {Mass=12140, CoreSize=16, BuildSize=128, Type="Dynamic", Class="L"},
 
     SpaceXS = {Mass=38.99, CoreSize=2, BuildSize=16, Type="Space", Class="XS"},
